fabe572cbc0d7c2ce7b97c9ed035ea2e70e9e1b0
huangziqi2009 <huang_ziqi@sina.cn>
2019-04-19 09:05:45 -0700

    rebuild log

--- hello.py
+++ hello.py
@@ -3,34 +3,5 @@
 
 
 if __name__ == '__main__'
+    # rebuild 
     print "hello world 1"
-    print "hello world 2"
-    # add feature-A
-    print "hello world 3"
-    print "hello world 4"
-    print "hello world 22222222222222222222" #print "hello world 1!"
-    # add feature-D
-    print "hello world 22222222222222222222" #print "hello world 2!"
-    print "hello world 22222222222222222222" #print "hello world 3!"
-
-    # add log 2222222222
-    print "hello world 1"
-    print "hello world 1"
-    print "hello world 1"
-    print "hello world 1"
-    print "hello world 1"
-    print "hello world 1"
-    print "hello world 1"
-    print "hello world 1"
-    print "hello world 1"
-    print "hello world 1"
-    print "hello world 1"
-    print "hello world 1"
-    print "hello world 22222222222222222222" #print "hello world 3!"
-
-    # add from 11111
-    print "hello world 22222222222222222222" #print "hello world 3!"
-    print "hello world 22222222222222222222" #print "hello world 3!"
-    print "hello world 22222222222222222222" #print "hello world 3!"
-    print "hello world 22222222222222222222" #print "hello world 3!"
-    print "hello world 22222222222222222222" #print "hello world 3!"
