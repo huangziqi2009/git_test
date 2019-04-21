3550d54532e9a9a024e60684499ce215d1b1fa3a
huangziqi2009 <huang_ziqi@sina.cn>
2019-04-21 05:35:51 -0700

    modify code.

--- hello.py
+++ hello.py
@@ -2,10 +2,5 @@
 #-*- coding:UTF-8 -*-
 
 if __name__ == '__main__':
-    # rebuild 
     print "hello world 1"
     print "hello world 2"
-    print "路漫漫其修远兮，吾将上下而求索！"
-    print "hello world 3"
-    print "hello world 4"
-    print "hello world 5"
