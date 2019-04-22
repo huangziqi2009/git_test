f73479c8bfc081dd6a406841bd6bc5f4680d05d8
huangziming2015 <49773390+huangziming2015@users.noreply.github.com>
2019-04-22 14:42:05 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -5,7 +5,11 @@ if __name__ == '__main__':
     print "hello world 1"
     print "hello world 2"
 
-    print "hello world 3"
+    print "hello world 3000"
     print "hello world 4"
     print "hello world 5"
     print "hello world 6"
+    
+    print "hello world 7"
+    print "hello world 8"
+    print "hello world 9"
